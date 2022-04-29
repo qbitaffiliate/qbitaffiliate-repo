ecbd000b81304d0f93cb4f32aa05b54d4b463fad
qbitaffiliate <104616114+qbitaffiliate@users.noreply.github.com>
2022-04-29 13:42:18 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,7 +10,7 @@ from sys import executable
 from telegram import ParseMode, InlineKeyboardMarkup
 from telegram.ext import CommandHandler
 
-from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, alive, AUTHORIZED_CHATS, LOGGER, Interval, rss_session
+from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, alive, AUTHORIZED_CHATS, OWNER_ID, TIMEZONE, LOGGER, Interval, rss_session
 from .helper.ext_utils.fs_utils import start_cleanup, clean_all, exit_clean_up
 from .helper.telegram_helper.bot_commands import BotCommands
 from .helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage, sendLogFile
